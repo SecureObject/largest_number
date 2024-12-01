1f54bc41ad3270780c8811ba6aeda837ca069dd2
Rajat <unsafeobject@gmail.com>
2024-12-01 10:11:46 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-/* This program will take 3 arguments through CLI and print the largest number among the numbers
+/* This program will take 3 arguments through CLI and print the largest number among the three numbers
  
 use syntax  =====>  cargo new -- arg1 arg2 arg5
 */
