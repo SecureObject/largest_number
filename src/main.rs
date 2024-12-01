ef92c7e4c9b66bbfddf93fa7a7f1001da21a3200
ec <rajatdagarcool001@gmail.com>
2024-11-30 22:39:34 -0600

    largest_number

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+/* This program will take 3 arguments through CLI and print the largest number among the numbers
+ 
+use syntax  =====>  cargo new -- arg1 arg2 arg5
+*/
+
+
+
+use std::env::{Args, args};
+
+fn main(){
+    let mut args: Args = args();
+
+    let first = args.nth(1).unwrap().parse::<i32>().unwrap();
+    let second = args.nth(0).unwrap().parse::<i32>().unwrap();
+    let third = args.nth(0).unwrap().parse::<i32>().unwrap();
+    
+    println!("{}",largest_num(first, second, third));
+}
+
+fn largest_num(first: i32, second: i32, third: i32) -> String{
+
+   if first > second && first > third{
+        format!("first is largest number {}",first)
+    }else if second > first && second > third{
+        format!("second is largest number {}",second)
+    }else {
+        format!("third is largest number {}", third)
+    }
+}
